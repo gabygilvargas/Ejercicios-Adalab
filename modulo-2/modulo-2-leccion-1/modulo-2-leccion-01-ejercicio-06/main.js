24337ca5a058be30b1fd5c077d7f9a9362c3f6a9
Gabriela Gil <gabygvargas@hotmail.com>
2021-08-04 18:40:27 +0200

    exercise 6

---
+++ modulo-2/modulo-2-leccion-1/modulo-2-leccion-01-ejercicio-06/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const pruebaElement = document.querySelector('.prueba');
+
+const content = '<h1>Lorem ipsum</h1><img src="http://via.placeholder.com/350x150"></img><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>';
+
+pruebaElement.innerHTML = content;
--- modulo-2/modulo-2-leccion-1/modulo-2-leccion-01-prueba/main.js
+++ modulo-2/modulo-2-leccion-1/modulo-2-leccion-01-prueba/main.js
@@ -5,6 +5,13 @@ document.querySelector('h1').innerHTML = '¡Esta página Hola mundo!';
 // Aquí va el código JavaScript 
 
 
+//creamos 1 constante donde se guardará el listado  
+const listElement = document.querySelector('.list');
 
 
+//creamos una constante donde estaran los li home y contact
+const content = '<li><a href="#">Home</a></li><li><a href="#">Contact</a></li>';
+
+//en la constante de listado metemos los li
+listElement.innerHTML = content;
 
