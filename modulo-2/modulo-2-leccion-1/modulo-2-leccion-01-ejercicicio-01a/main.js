5e13b19270d17154366b813d2e9966a63d98aef3
Gabriela Gil <gabygvargas@hotmail.com>
2021-08-04 08:05:13 +0200

    create ejercice

---
+++ modulo-2/modulo-2-leccion-1/modulo-2-leccion-01-ejercicicio-01a/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+document.querySelector('h1').innerHTML = '¡Warning! Esta página no es compatible con la versión actual de tu navegador. Por favor actualizalo a una versión más reciente!';
+
+// Aquí va el código JavaScript 
+
+
+
+
+
---
+++ modulo-2/modulo-2-leccion-1/modulo-2-leccion-01-prueba/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+document.querySelector('h1').innerHTML = '¡Esta página Hola mundo!';
+
+// Aquí va el código JavaScript 
+
+
+
+
+
